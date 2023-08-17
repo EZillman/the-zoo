d3f43ac078e9b29cc0dcaa37136e2eaee0a77835
EZillman <evelina.zillman@medieinstitutet.se>
2023-08-17 22:19:47 +0200

    added interface

---
+++ src/models/IAnimal.ts
@@ -0,0 +1,9 @@
+export interface IAnimal {
+    id: number;
+    name: string;
+    yearOfBirth: number;
+    shortDescription: string;
+    imageUrl: string;
+    isFed: boolean;
+    lastFed: string;
+}
